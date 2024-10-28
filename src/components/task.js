5da8f0822c3d34d5f96529d356513e15c067792a
Sh3tys <titouanparzival@gmail.com>
2024-10-28 10:26:04 +0100

    Remove code main towards task.js and add isTextValid()

--- src/components/task.js
+++ src/components/task.js
@@ -1,67 +1,65 @@
-function addTask() {
-    let taskInputText = document.getElementById("name-task");
-    let taskText = taskInputText.value;
-
-    let taskInputDate = document.getElementById("date-task");
-    let taskDate = taskInputDate.value;
-    
-    if (taskText === "" || taskDate === ""){
-        alert("Please enter name task or date task !");
-        return;
-    };
-
-    let taskList = document.getElementById("task-list");
-
-    let li = document.createElement("li");
-    li.id = "task";
-    taskDate = taskDate.split("-").reverse().join("-");
-    li.textContent = taskText + "   " +taskDate;
-
-    let deleteButton = document.createElement("button");
-    deleteButton.textContent = "Delete";
-    deleteButton.style.display = "none";
-    li.appendChild(deleteButton);
-
-    let checkBox = document.createElement("input");
-    checkBox.type = "checkbox";
-    
-    let finishList = document.getElementById("finish-task");
-    checkBox.onchange = function () {
-        if (checkBox.checked) {
-            deleteButton.style.display = "inline";
-            finishTask(taskList, finishList, li);
-        } else {
-            deleteButton.style.display = "none";
-            returnTask(taskList, finishList, li); // Appeler returnTask si décochée
-        }
-    };
-    
-    deleteButton.onclick = function () {
-        deleteTask(finishList, li);
-    };
-    
-    li.appendChild(checkBox);
-    taskList.appendChild(li);
-    
-
-    taskInputText.value = "";
-    taskInputDate.value = "";
-};
-
-function deleteTask(finishList, li) {
-            
-    finishList.removeChild(li);
-
-};
-
-function finishTask(taskList, finishList, li) {
-    finishList.appendChild(li);
-    taskList.removeChild(li);
-
-};
-
-function returnTask(taskList, finishList, li) {
-    taskList.appendChild(li);
-    finishList.removeChild(li);
+function isTextValid(text) {
+  if (text.trim() === "") {
+    alert("Please enter name task or date task !");
+    return true;
+  }
+  return false;
 }
 
+function addTask() {
+  let taskInputText = document.getElementById("name-task");
+  let taskText = taskInputText.value;
+
+  let taskInputDate = document.getElementById("date-task");
+  let taskDate = taskInputDate.value;
+
+  let taskList = document.getElementById("task-list");
+
+  let li = document.createElement("li");
+  li.id = "task";
+  taskDate = taskDate.split("-").reverse().join("-");
+  li.textContent = taskText + "   " + taskDate;
+
+  let deleteButton = document.createElement("button");
+  deleteButton.textContent = "Delete";
+  deleteButton.style.display = "none";
+  li.appendChild(deleteButton);
+
+  let checkBox = document.createElement("input");
+  checkBox.type = "checkbox";
+
+  let finishList = document.getElementById("finish-task");
+  checkBox.onchange = function () {
+    if (checkBox.checked) {
+      deleteButton.style.display = "inline";
+      finishTask(taskList, finishList, li);
+    } else {
+      deleteButton.style.display = "none";
+      returnTask(taskList, finishList, li); // Appeler returnTask si décochée
+    }
+  };
+
+  deleteButton.onclick = function () {
+    deleteTask(finishList, li);
+  };
+
+  li.appendChild(checkBox);
+  taskList.appendChild(li);
+
+  taskInputText.value = "";
+  taskInputDate.value = "";
+}
+
+function deleteTask(finishList, li) {
+  finishList.removeChild(li);
+}
+
+function finishTask(taskList, finishList, li) {
+  finishList.appendChild(li);
+  taskList.removeChild(li);
+}
+
+function returnTask(taskList, finishList, li) {
+  taskList.appendChild(li);
+  finishList.removeChild(li);
+}
