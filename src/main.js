5200c26e35e55873d0d154d4a77b880d134c37e3
Max Reis <maxreis507@gmail.com>
2024-10-29 10:04:33 +0100

    Add menu-list feat

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
 import { addTask } from "./components/task.js";
+import { addList } from "./components/menu-list.js";
 
 document.getElementById("add-task-button").addEventListener("click", addTask);
+addList()
