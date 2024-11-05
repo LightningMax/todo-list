53f03d49e79199b7cc01d4ee745f58500b2ba924
Solofoniaina Armando <armandodapa10@gmail.com>
2024-11-05 12:37:39 +0100

    Add todo comments

---
+++ src/components/modify-task.js
@@ -0,0 +1,10 @@
+/* 
+    TODO:
+      Refact: use onclick instead of addEventListener when
+      working with buttons.
+      Ex: showAllTasksButton.onclick = ...
+      Refact: use textContent insteado of innerHTML when
+      adding text to a element
+      Refact: The showAllTasks in fact should show only the tasks
+      from one list.
+*/
