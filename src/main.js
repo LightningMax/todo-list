1760b5cd9ebebf15664d2adb38ea5294e18e6097
Max Reis <maxreis507@gmail.com>
2024-11-05 20:06:11 +0100

    Reformat code in main.js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,10 @@
 import { Lists } from "./components/menu-list.js";
 
-
 class App {
-    constructor(lists) {
-        this.lists = lists
-    }
+  constructor(lists) {
+    this.lists = lists;
+  }
 }
 
-const lists = new Lists()
-const app = App(lists)
+const lists = new Lists();
+const app = App(lists);
