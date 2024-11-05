6f66b3db1647fe8998b10aa99d0784378253756c
Max Reis <maxreis507@gmail.com>
2024-11-05 20:01:11 +0100

    Add app class to encapsulate every feature

--- src/main.js
+++ src/main.js
@@ -1,3 +1,11 @@
 import { Lists } from "./components/menu-list.js";
 
+
+class App {
+    constructor(lists) {
+        this.lists = lists
+    }
+}
+
 const lists = new Lists()
+const app = App(lists)
